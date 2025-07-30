03bedccbd64dff24ee2f163ead5e3f3abd5ed662
Quang Duy <dinhduy2012003@gmail.com>
2025-07-31 06:43:35 +0700

    fix: fetch api from production

--- static/script.js
+++ static/script.js
@@ -65,7 +65,7 @@ startBtn.addEventListener("click", async () => {
     const formData = new FormData();
     formData.append("file", selectedFile);
 
-    const response = await fetch("http://localhost:8000/upload", {
+    const response = await fetch("http://captionify-app.onrender.com/upload", {
       method: "POST",
       body: formData,
     });
